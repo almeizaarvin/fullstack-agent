8104a6dddb2d8c26c00bf4a20a9432b0beda938f
almeizaarvin <13519066@std.stei.itb.ac.id>
2024-06-05 16:16:53 +0700

    Add landing page body

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+    const [category, setCategory] = useState('All');
+
   return (
-    <div>
+    <div className='center'>
         <Header />
+        <ExploreMenu category={category} setCategory={setCategory}/>
+        <FoodDisplay category={category}/>
     </div>
   )
 }
